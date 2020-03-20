9d70abd3b7b31d4940989c4122ac12d631d7c33e
zhouxiang19910319 <zhouxiang19910319@gmail.com>
2020-03-20 21:17:19 +0800

    finish feature discovery panel

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Collapsible Init
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, options);
   });
 
 // Expandable Collapse
@@ -24,8 +23,29 @@ window.addEventListener("DOMContentLoaded", event => {
 });
 
 // Feature Discovery
+
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.tap-target');
-  var instances = M.TapTarget.init(elems, options);
-  var instance = M.TapTarget.getInstance(elem);
+  M.TapTarget.init(elems);
 });
+
+function next(n) {
+var inst;
+
+// Get each of the elements
+var elems = document.querySelectorAll(".tap-target");
+
+var current = n;
+var prev = --n;
+
+// If a previous target is open, close it.
+if(prev >= 0) {
+  inst = M.TapTarget.getInstance(elems[prev]);
+  inst.close();
+  inst.destroy();
+}
+
+// Then, open the new target
+inst = M.TapTarget.getInstance(elems[current]);
+inst.open();
+}
